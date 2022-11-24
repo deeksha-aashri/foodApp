1e6aa0d4635a6deaee2aaf8f03352b56c91d17e4
deeksha-aashri <deekshaaashri1@gmail.com>
2022-11-24 17:50:07 +0530

    :sparkles: User operations in usercontroller.js done

--- controller/userController.js
+++ controller/userController.js
@@ -1,73 +1,102 @@
-async function getUsers(req, res) {
-    console.log(req.query);
-    // let { name, age } = req.query;
-    // let filteredData=user.filter(userObj => {
-    //     return (userObj.name==name && userObj.age==age)
-    // })
-    // res.send(filteredData);
-  
-    //get all users from db
-    let allUsers = await userModel.find();
-  
-    res.json({ msg: "users retrieved", allUsers });
-    // console.log("getUser called ");
-    // next();
-  }
-  
-  function postUser(req, res) {
-    console.log(req.body.Name);
-    //then i can put this in db
-    user.push(req.body);
+const userModel = require("../models/userModel");
+
+module.exports.getUser = async function (req, res) {
+  try {
+    let id = req.id;
+    let user = await userModel.findById(id);
+
+    res.json({ msg: "users retrieved", user });
+  } catch (err) {
     res.json({
-      message: "Data received successfully",
-      user: req.body,
+      msg: err.message,
     });
   }
-  
-  async function updateUser(req, res) {
-    console.log(req.body);
-    let dataToBeUpdated = req.body;
-    // for (key in dataToBeUpdated) {
-    //   user[key] = dataToBeUpdated[key];
-    // }
-    let doc = await userModel.findOneAndUpdate(
-      { email: "abc@gmail.com" },
-      dataToBeUpdated
-    );
+};
+
+// module.exports.postUser=function (req, res) {
+//   console.log(req.body.Name);
+//   //then i can put this in db
+//   user.push(req.body);
+//   res.json({
+//     message: "Data received successfully",
+//     user: req.body,
+//   });
+// }
+
+module.exports.updateUser = async function (req, res) {
+  console.log(req.body);
+  let id = req.params.id;
+  let user = await userModel.findById(id);
+  let dataToBeUpdated = req.body;
+  // {
+  //   name: "Abhi",
+  //   email: 'abc@gmail.com'
+  // }
+  try {
+    if (user) {
+      const keys = []; //['name','email]
+      for (let key in dataToBeUpdated) {
+        keys.push(key);
+      }
+
+      for (let i = 0; i < keys.length; i++) {
+        user[keys[i]] = dataToBeUpdated[keys[i]];
+        //name=Abhi
+      }
+
+      const updatedData = await user.save();
+      res.json({
+        message: "data updated succesfully",
+        updatedData,
+      });
+    } else {
+      res.json({
+        message: "user not found",
+      });
+    }
+  } catch (err) {
     res.json({
-      message: "data updated succesfully",
+      message: err.message,
     });
   }
-  
-  async function deleteUser(req, res) {
-    // user = {};
+};
+
+module.exports.deleteUser = async function (req, res) {
+  try {
+    let id = req.params.id;
     // let doc = await userModel.deleteOne({ email: "abcd@gmail.com" });
     // let doc = await userModel.findOneAndRemove({ email: "abcde@gmail.com" });
-    let user = await userModel.findOne({ email: "abc@gmail.com" });
-    console.log(user);
-    let del = await user.remove();
-    console.log(del);
+    let user = await userModel.findByIdAndDelete(id);
+    if(user){
+        res.json({
+            msg: "user has been deleted",
+            user,
+          });
+    }
+    else{
+        res.json({
+            msg: "user has not been found",
+            user,
+          });
+    }
+    
+  } catch (err) {
     res.json({
-      msg: "user has been deleted",
+      msg: err.message,
     });
   }
-  
-  function getUserById(req, res) {
-    console.log(req.params.name);
-    //let {id}=req.params;
-    // let user = db.findOne(id);
-    res.json({ msg: "user id is ", obj: req.params });
+};
+
+module.exports.getAllUser = async function (req, res) {
+  try {
+    let allUsers = await userModel.find();
+    res.json({
+      msg: "user id is ",
+      allUsers,
+    });
+  } catch (err) {
+    res.json({
+      msg: err.message,
+    });
   }
-  
-  function setCookies(req, res) {
-    // res.setHeader('Set-Cookie', 'isLoggedIn=true');
-    res.cookie("isLoggedIn", false, { maxAge: 10000, secure: true });
-    res.cookie("password", 12345678, { secure: true });
-    res.send("cookies has been set");
-  }
-  
-  function getCookies(req, res) {
-    let cookies = req.cookies;
-    console.log(cookies);
-    res.send("cookies received");
-  }
+};
